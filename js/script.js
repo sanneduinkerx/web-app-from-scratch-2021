787f8467477a2ade7c7300a4be9b2f69b50508d1
sanneduinkerx <sanne@duinker.eu>
2021-02-02 22:16:09 +0100

    data inladen

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 var url = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=queen&api_key=9445b881096d29d7c6de9f9d2eb6b50d&format=json';
 
 var request = new XMLHttpRequest();
+
 request.open('GET', url, true);
 request.send();
 
@@ -13,12 +14,18 @@ request.send();
 // }; 
 
 // met arrow functie 
-request.onload = (topAlbums => {
+request.onload = ( topAlbums => {
     var data = JSON.parse(request.response);
+    var p = document.querySelector('p');
+
     console.log(data);
+    
+    p.textContent = data.topalbums.album[1].name;
+   
 });
 
-var p = document.querySelector('p');
+
+
 
 
 
