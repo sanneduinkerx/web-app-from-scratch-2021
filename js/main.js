dc98756c5c425da3ab99905ea6ec829901b9d6f4
sanneduinkerx <sanne@duinker.eu>
2021-02-17 19:09:22 +0100

    routie implemented

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-// import { showResults } from './modules/render.js';
 import { searchArtist } from './modules/search.js';
-
-start();
+import { router } from './modules/routes.js';
 
 function start() {
-
     const searchResult = searchArtist();
-    //showResults(searchResult); 
 }
+
+function router() {
+    routie('album/:albumName', function(albumName){
+        console.log(albumName);
+    });
+}
+
+start();
+router();
+
