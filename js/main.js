dacf96e4e62041930c9091fcb4d5fa1c39d2977f
sanneduinkerx <sanne@duinker.eu>
2021-02-26 19:14:41 +0100

    main changed - search function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 import { detailPage } from './modules/renderDetail.js';
 import { searchArtist } from './modules/search.js';
 
-function router() {
+searchArtist();
+router();
 
+function router() {
     // when the user clicks on an album and the window browser has an #album/albumName, that function gets called.
     routie ('album/:albumName', detailPage);   
 };
 
-router();
 
