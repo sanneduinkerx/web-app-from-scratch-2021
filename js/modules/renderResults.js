ee9052a5263e46940dc7d583f1359365819be59f
sanneduinkerx <sanne@duinker.eu>
2021-02-28 14:29:23 +0100

    small changes in names

--- js/modules/renderResults.js
+++ js/modules/renderResults.js
@@ -1,7 +1,7 @@
 // function which shows the results from the user search
 export function showResults(data, section, artistName){
 
-    const topAlbum = data.topalbums.album;
+    // const topAlbum = data.topalbums.album;
     const searchResult = document.createElement('p');
 
     // feedback for user, knows where he/she searched for
@@ -9,7 +9,7 @@ export function showResults(data, section, artistName){
     section.appendChild(searchResult);
 
     // for each album, an article with the name and album image from the API
-    topAlbum.forEach((albums) => {
+    data.forEach((albums) => {
 
             // creating elements in HTML doc for the data  
             const article = document.createElement('article');
