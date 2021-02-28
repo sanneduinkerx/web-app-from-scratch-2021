6aa337f0e2b745f57023eab279a59979c442ec74
sanneduinkerx <sanne@duinker.eu>
2021-02-28 14:26:39 +0100

    api fetch - asyn and await added

--- js/modules/api.js
+++ js/modules/api.js
@@ -1,9 +1,14 @@
 // fetching api with given URL 
-export function getApiData(url) {
+export async function getApiData(url) {
 
-    // getting the data with fetch: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    return fetch(url)
+    // getting the data with fetch
+    //source: https://stackoverflow.com/questions/59394620/why-fetch-returns-promise-pending
+    // let: because data changes when given a different URL
+    // await:
+    let apiData = await fetch(url)
         .then(response => response.json())
         // if url doesnt match or couldnt fetch api it sends an error message
-        .catch((err) => console.log('error')); 
+        .catch((err) => console.log(err)); 
+
+    return apiData;
  };
