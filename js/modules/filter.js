355bd3fb0d2b7599337c99475d77c8abe777518f
sanneduinkerx <sanne@duinker.eu>
2021-02-28 18:01:42 +0100

    error message added

--- js/modules/filter.js
+++ js/modules/filter.js
@@ -1,3 +1,8 @@
-export function filter(data) {
-    console.log(data);
+import { showResults } from './renderResults.js';
+import {section} from './search.js';
+
+export function filter(apiData) {
+    //object.values, source: https://stackoverflow.com/questions/55458675/filter-is-not-a-function
+    const filteredData = Object.values(apiData.topalbums.album).filter(noImg => noImg.image[3]['#text'] != "");
+    showResults(filteredData, section);
 }
--- js/modules/renderDetail.js
+++ js/modules/renderDetail.js
@@ -5,37 +5,54 @@ export async function detailPage(albumName) {
 
     //new method and artistName and new URL to get album details, 
     const artistName = searchInput.value;
-    const methodGetinfo = 'album.getinfo'; 
-    const urlAlbumInfo = `${endpoint}${methodGetinfo}&api_key=${apiKey}&artist=${artistName}&album=${albumName}&format=json`; 
+    if(artistName != ""){
+        const methodGetinfo = 'album.getinfo'; 
+        const urlAlbumInfo = `${endpoint}${methodGetinfo}&api_key=${apiKey}&artist=${artistName}&album=${albumName}&format=json`; 
 
-    console.log(urlAlbumInfo);
-    section.innerHTML = ''; 
+        console.log(urlAlbumInfo);
+        section.innerHTML = ''; 
 
-    //
-    const apiData = await getApiData(urlAlbumInfo);
-
-    // if(apiData.album.wiki.content){
-    //     render(apiData);
-    // } else(!apiData.album.wiki.content) {
-    //     dataNotFound();
-    // };
-    render(apiData);
-    // getApiData(urlAlbumInfo)
-    //     .then(data => {render(data)})
+        //fetch api data, details from an album
+        const apiData = await getApiData(urlAlbumInfo);
+        
+        if(apiData.album.wiki == undefined)  {
+            dataNotFound();
+        } else{
+            renderDetailPage(apiData);
+        }
+    }
 };
 
-function render(data){
-    console.log(data);
-
+function renderDetailPage(data){
     // create elements
     const p = document.createElement('p');
     const cover = document.createElement('img');
     const title = document.createElement('h1');
     const div = document.createElement('div');
+    const a = document.createElement('a');
+    
+    // splitting and retrieving pieces from string //
+    // source: https://www.w3schools.com/js/js_string_methods.asp
+    const summaryText = data.album.wiki.summary;
+
+    //finding the position where the link element starts in string summary 
+    const linkPosition = summaryText.lastIndexOf("<a href");
+
+    const summaryStr = summaryText.substr(0,linkPosition);
+    const linkStr = summaryText.substr(linkPosition);
+
+    //split the retrieved link in 3 with " - to get de a href link and text content from a href
+    const stringArray = linkStr.split('"');
+    const link2Position = stringArray[2].indexOf("</a>");
+    const linkTxt = stringArray[2].substr(1, link2Position - 1);
+    
+    // fill elements with the string content retrieved above
+    p.textContent = summaryStr;
+    a.href = stringArray[1];
+    a.textContent = linkTxt;
 
     // fill elements with content from API
     section.classList.add('Albuminfo');
-    p.textContent = data.album.wiki.content;
     cover.src = data.album.image[4]['#text'];
     title.textContent = `${data.album.artist} - ${data.album.name}`;
 
@@ -44,8 +61,13 @@ function render(data){
     div.appendChild(title);
     div.appendChild(cover);
     div.appendChild(p);
+    div.appendChild(a);
 };
 
-// function dataNotFound(){
-//     console.log('Data not found');
-// }
+//error state - when there is no data to be shown
+function dataNotFound(){
+    const errorMessage = document.createElement('p');
+
+    errorMessage.textContent = "We couldn't find any information about this album. Please try again later.";
+    section.appendChild(errorMessage);
+}
