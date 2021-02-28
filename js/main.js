956f94cbf34dd1499b14dcff2be84fce957fa192
sanneduinkerx <sanne@duinker.eu>
2021-02-28 14:29:43 +0100

    routes in main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 import { detailPage } from './modules/renderDetail.js';
 import { searchArtist } from './modules/search.js';
 
-searchArtist();
 router();
 
 function router() {
+    routie ('', searchArtist())
     // when the user clicks on an album and the window browser has an #album/albumName, that function gets called.
-    routie ('album/:albumName', detailPage);   
+    routie ('album/:albumName', detailPage);  
 };
 
 
